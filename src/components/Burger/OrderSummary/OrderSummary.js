732baf0f202b5024cc32a13a9c593f2ff595f1bb
Salih Onur YÖNT <onur.yont@gmail.com>
2020-01-29 17:39:13 +0300

    Added buttons to Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Auxiliary'
+import Button from '../../UI/Button/Button'
 
 const orderSummary = (props) => {
 
@@ -19,6 +20,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
+            <Button clicked={props.purchaseCanceled} btnType="Danger">CANCEL</Button>
+            <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
         </Aux>
     );
 }
